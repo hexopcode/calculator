c6309fec33e712bbf819f653d52bdfae33787180
Rui Lopes <ruilopes@mac-mini.lan>
2020-07-17 21:54:39 -0400

    Add toString() method to Callable.

--- interpreter/callable.ts
+++ interpreter/callable.ts
@@ -3,4 +3,8 @@ export type CallableArity = ([number] | [number, number]);
 export abstract class Callable {
     abstract call(args: any[]): any;
     abstract arity(): CallableArity;
+
+    toString(): string {
+        return '<native>';
+    }
 }
