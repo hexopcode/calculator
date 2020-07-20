e8320806f9688b1f9af5971742eb5e1212f3354c
Rui Lopes <ruilopes@mac-mini.lan>
2020-07-19 20:29:56 -0400

    Switch the interpreter to use boxed Values instead of raw literals.
    This gets us better type guarantees when evaluating expressions, as well as opens the path for non-number value types.

--- interpreter/callables/native.ts
+++ interpreter/callables/native.ts
@@ -1,10 +1,13 @@
 import {Callable, CallableArity} from './callable';
 import {Executor} from '../executor';
+import {Value} from '../values/value';
+import {NumberValue} from '../values/typed';
 
 export function __builtin__(fn: Function, minArgs: number = 1, maxArgs?: number): Callable {
     return new class extends Callable {
-        call(args: any[], _xecutor: Executor): any {
-            return fn(...args);
+        call(args: Value<any>[], _xecutor: Executor): Value<any> {
+            const unboxed = args.map(arg => arg.assertNumber());
+            return new NumberValue(fn(...unboxed));
         }
 
         arity(): CallableArity {
--- interpreter/executor.ts
+++ interpreter/executor.ts
@@ -1,7 +1,8 @@
 import {Environment} from './environment';
 import {Expr} from './parser/expr';
+import {Value} from './values/value';
 
 export interface Executor {
-    evaluateWithEnvironment(expr: Expr, environment: Environment): any;
+    evaluateWithEnvironment(expr: Expr, environment: Environment): Value<any>;
     environment(): Environment;
 }
