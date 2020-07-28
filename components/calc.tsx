a182439a2870cd2c8bb9c03e14770dd7f6e1a049
Rui Lopes <ruidlopes@gmail.com>
2020-07-27 22:17:21 -0400

    Rename a few things in calc.tsx to more meaningful names.

--- components/calc.tsx
+++ components/calc.tsx
@@ -6,7 +6,6 @@ import {Input} from './input';
 import {Interpreter, InterpreterResult, InterpreterResultType} from '../interpreter/interpreter';
 import {Screen} from './screen';
 import {Stmt} from '../interpreter/parser/stmt';
-import {Value} from '../interpreter/values/value';
 
 
 type CalcProps = {};
@@ -21,7 +20,7 @@ export class Calc extends React.Component {
 
     async componentDidMount() {
         const interpreterResult = await this.interpreter.createEnvironment();
-        this.renderInterpreterResult(interpreterResult, false);
+        this.updateInterpreterResult(interpreterResult, false);
     }
 
     render() {
@@ -33,14 +32,14 @@ export class Calc extends React.Component {
         );
     }
 
-    renderInterpreterResult(interpreterResult: InterpreterResult, renderResults: boolean = true) {
+    updateInterpreterResult(interpreterResult: InterpreterResult, includeResults: boolean = true) {
         const lines = interpreterResult.all.map((result: InterpreterResultType) => {
             if (result instanceof ErrorValue) {
                 return result.toString();
             } else if ((result as Stmt).accept) {
                 return new AstPrinter().print(result as Stmt);
             } else {
-                return renderResults ? result.toString() : null
+                return includeResults ? result.toString() : null
             }
         }).filter(result => result !== null);
 
@@ -49,6 +48,6 @@ export class Calc extends React.Component {
 
     async handleInput(text: string) {
         const interpreterResult = await this.interpreter.run(text);
-        this.renderInterpreterResult(interpreterResult);
+        this.updateInterpreterResult(interpreterResult);
     }
 }
